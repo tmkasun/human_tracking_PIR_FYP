c64fe655058fae88722ed2e8ca104639b512c9db
tmkasun <tmkasun@gmail.com>
2015-08-15 23:41:26 +0530

    Reduce resolution in internal camera capture frames

--- grid_eye_server/camera_capture.py
+++ grid_eye_server/camera_capture.py
@@ -1,3 +1,5 @@
+from cv2.cv import CV_INTER_LINEAR
+
 __author__ = 'tmkasun'
 
 import numpy as np
@@ -5,15 +7,18 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = frame
+    r = 500.0 / gray.shape[1]
+    dim = (500, int(gray.shape[0] * r))
+    resized_frame = cv2.resize(gray, dim,interpolation=CV_INTER_LINEAR)
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', resized_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
